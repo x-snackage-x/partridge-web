503ba923f8605f626a09d023a25dc4fb6b59fddf
Karim El-Hayawan <snackage@gmail.com>
2026-03-13 21:32:21 +0100

    another approach

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default {
             "Cross-Origin-Embedder-Policy": "require-corp"
         },
         open: '/partridge.html',
+        base: '/<REPO>/',
     },
     build: {
         rollupOptions: {
